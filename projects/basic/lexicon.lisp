419c57fa9ae56ba7643c29c8c7e7b197141e9064
Umut Ozge <umut.oezge@gmail.com>
2024-03-08 10:55:46 +0300

    opr to b. lex.

--- projects/basic/lexicon.lisp
+++ projects/basic/lexicon.lisp
@@ -2,13 +2,14 @@
 
 ; proper names
 s/(s\np[sg])  : \p.p lex' < john mary
-; intransitive verbs
+; intransitive < verbs
 s\np[sg]      : \x.lex' x < talks
 s\np[pl]      : \x.lex' x < talk
 ; nouns
 n[sg]         : \x.lex' x < spy
 n[pl]         : \x.lex' x < spies
-; determiners
+; dete : rminers
 (s/(s\np))/n : \p\q. q (lex' p) < the
-(s/(s\np[sg]))/n[sg] : \p\q. q (lex' p) < a
-
+(s/(s\np[sg]))/n[sg] : \p\qEx. p x & q x  < some 
+(s/(s\np[sg]))/n[sg] : \p\qAx. p x > q x  < every
+(s/(s\np[sg]))/n[sg] : \p\q~Ex. p x & q x  < no 
